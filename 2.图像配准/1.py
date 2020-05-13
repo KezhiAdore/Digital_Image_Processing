81c8314ce49d46559c26ad7266ebaaadc2c38e5a
KezhiAdore <17702917181@189.cn>
2020-05-13 21:00:09 +0800

    修正错误

--- 2.图像配准/1.py
+++ 2.图像配准/1.py
@@ -1,30 +1,28 @@
 import cv2 as cv
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import mlab
-from matplotlib import rcParams
-def graybar(img,win_num,img_name,flag):
-    num=np.zeros((256))
-    img_shape=img.shape
-    if flag:
-        for i in range(img_shape[0]):
-            for j in range(img_shape[1]):
-                num[img[i][j][0]]+=1
-    else:
-        for i in range(img_shape[0]):
-            for j in range(img_shape[1]):
-                if img[i][j][0]==0:
-                    img[i][j][0]=255
-                num[img[i][j][0]]+=1
-    fig=plt.figure(win_num)
-    rects=plt.bar(range(256),num,0.2)
-    plt.title('gray_bar_'+img_name)
-    plt.show()
-def flag(str):
-    if '1' in str:
-        return 0
-    return 1
-filename=['citywall.bmp','citywall1.bmp','citywall2.bmp','elain.bmp','elain1.bmp','elain2.bmp','elain3.bmp','lena.bmp','lena1.bmp','lena2.bmp','lena3.bmp','woman.BMP','woman1.bmp','woman2.bmp']
-for i in filename:
-    img=cv.imread(i)
-    graybar(img,0,i,flag(i))
+import math
+#np.set_printoptions(threshold=np.inf)
+
+imgA=cv.imread("Image A.jpg")
+imgB=cv.imread("Image B.jpg")
+point_A=np.array([[1448,1694,1756,383,2290,2035,2150],[1308,1198,2744,2516,933,2693,1968],[1,1,1,1,1,1,1]])
+point_B=np.array([[1042,1252,1708,323,1761,1966,1890],[1077,907,2387,2519,498,2265,1535],[1,1,1,1,1,1,1]])
+H=np.dot(point_B,np.linalg.pinv(point_A))
+print(H)
+shape_A=imgA.shape
+shape_B=imgB.shape
+result,mistake=np.zeros(shape_A),np.zeros(shape_A)
+result=result.astype(np.uint8)
+mistake=mistake.astype(np.uint8)
+for i in range(shape_A[0]):
+    for j in range(shape_A[1]):
+        address=np.dot(H,np.array([i,j,1]))
+        address=address.astype(np.int)
+        if address[0]>0 and address[0]<shape_B[0] and address[1]>0 and address[1]<shape_B[1]:
+            result[i][j]=imgB[address[0]][address[1]]
+            mistake[i][j]=imgA[i][j]-result[i][j]
+cv.namedWindow("test",cv.WINDOW_NORMAL)
+cv.imshow("test",mistake)
+cv.waitKey(0)
+cv.imwrite("result.jpg",result)
+cv.imwrite("mistake.jpg",mistake)
